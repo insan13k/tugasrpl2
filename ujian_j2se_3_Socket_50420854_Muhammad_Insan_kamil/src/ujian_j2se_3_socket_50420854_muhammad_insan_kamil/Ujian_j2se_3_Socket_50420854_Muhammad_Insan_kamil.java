101f079f2dcdc483cc21933b2808c88be3b760da
insan13k <142216857+insan13k@users.noreply.github.com>
2023-12-04 09:28:51 +0700

    Add files via upload

---
+++ ujian_j2se_3_Socket_50420854_Muhammad_Insan_kamil/src/ujian_j2se_3_socket_50420854_muhammad_insan_kamil/Ujian_j2se_3_Socket_50420854_Muhammad_Insan_kamil.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ujian_j2se_3_socket_50420854_muhammad_insan_kamil;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Ujian_j2se_3_Socket_50420854_Muhammad_Insan_kamil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
